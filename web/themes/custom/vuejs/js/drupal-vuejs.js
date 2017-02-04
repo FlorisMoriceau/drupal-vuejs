6b7535acd13b058fb8a0b7fcbcd0165a1d4af31c
yann <yann.boisselier@gmail.com>
2017-02-04 16:45:15 +0100

    save

--- web/themes/custom/vuejs/js/drupal-vuejs.js
+++ web/themes/custom/vuejs/js/drupal-vuejs.js
@@ -1,6 +1,24 @@
-var app = new Vue({
-    el: '#drupal-vuejs',
-    data: {
-        message: 'Hello Vue!'
+
+Vue.component('front-list', {
+  data:function() {
+    return {
+      articles:[
+        {
+          title:"titre 1"
+        },
+        {
+          title:"titre 2"
+        },
+        {
+          title:"titre 3"
+        }
+      ]
     }
-})
+  },
+  template: '<ul><li v-for="article in this.articles">{{ article.title }}</li></ul>'
+});
+
+var app = new Vue({
+  el: '#drupal-vuejs',
+  template:"<front-list />"
+});
