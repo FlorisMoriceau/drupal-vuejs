c6869572070e3e995effd2c51a85d483ac7f895c
yann <yann.boisselier@gmail.com>
2017-02-04 20:50:33 +0100

    en essayant de tout mettre au dessus du répertoire web

---
+++ vuejs/ArticlesListContainer.vue
@@ -0,0 +1,22 @@
+<script>
+  import ArticlesList from "./ArticlesList.vue"
+  export default{
+    data:function() {
+      return {
+        articles:[]
+      }
+    },
+    created:function() {
+      this.$http.get('/api/articles').then(function(response) {
+        this.articles = response.body;
+      });
+    },
+    components:{ArticlesList}
+  }
+</script>
+
+<template>
+  <articles-list :articles="articles" />
+</template>
+
+
--- vuejs/main.js
+++ vuejs/main.js
@@ -1,22 +1,13 @@
+// Libraries and plugins
 import Vue from 'vue'
 import VueResource from 'vue-resource'
-import VueRouter from 'vue-router'
-
-import FrontList from './FrontList.vue'
-
 Vue.use(VueResource);
 
+// Our custom components
+import ArticlesListContainer from './ArticlesListContainer.vue'
+
+// Mount points
 new Vue({
   el: '#drupal-vuejs',
-  data:{
-    articles:[],
-  },
-  mounted:function() {
-    this.$http.get('/api/articles').then(function(response) {
-      this.articles = response.body;
-    });
-  },
-  components:{
-    FrontList
-  }
+  components:{ArticlesListContainer}
 });
